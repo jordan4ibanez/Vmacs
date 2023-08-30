095bfa9fddae662cc3e949ebd527d833b82bb83f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-30 11:55:57 -0400

    DOOOOOM

--- init.el
+++ init.el
@@ -16,8 +16,30 @@
 (line-number-mode 1)
 (global-display-line-numbers-mode 1)
 
+;; We gotta start with the theme maaaan. Doom-one is the absolute nicest out there.
 
-;; Testing sublimity
+(package-install 'doom-themes)
+
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-one t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+
+;; Testing sublimity - This causes weird issues
 
 ; (package-install 'sublimity)
 
