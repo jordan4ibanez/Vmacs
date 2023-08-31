64e58e617e53fc3fea9908246940d3cffd2b18be
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-31 12:50:39 -0400

    Bring filtering out the main dashboard

--- init.el
+++ init.el
@@ -115,9 +115,35 @@
 (centaur-tabs-enable-buffer-alphabetical-reordering)
 (setq centaur-tabs-adjust-buffer-order t)
 
-;; Just dump EVERYTHING into a single group.
-; (defun centaur-tabs-buffer-groups ()
-;   (list (cond (t "vmacs"))))
+;; Stop someone from accidentally closing the dashboard.
+(defun centaur-tabs-hide-tab (x)
+  "Do no to show buffer X in tabs."
+  (let ((name (format "%s" x)))
+    (or
+     ;; Current window is not dedicated window.
+     (window-dedicated-p (selected-window))
+
+     ;; Buffer name not match below blacklist.
+     (string-prefix-p "*epc" name)
+     (string-prefix-p "*helm" name)
+     (string-prefix-p "*Helm" name)
+     (string-prefix-p "*Compile-Log*" name)
+     (string-prefix-p "*lsp" name)
+     (string-prefix-p "*company" name)
+     (string-prefix-p "*Flycheck" name)
+     (string-prefix-p "*tramp" name)
+     (string-prefix-p " *Mini" name)
+     (string-prefix-p "*help" name)
+     (string-prefix-p "*straight" name)
+     (string-prefix-p " *temp" name)
+     (string-prefix-p "*Help" name)
+     (string-prefix-p "*mybuf" name)
+     (string-prefix-p "*dashboard*" name)
+
+     ;; Is not magit buffer.
+     (and (string-prefix-p "magit" name)
+          (not (file-name-extension name)))
+     )))
 
 ;; Treemacs
 (use-package treemacs
