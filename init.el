2c9c4e54d92036d45be72d2d71564b7105bc10e4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-09-02 06:10:29 -0400

    Finally fix this

--- init.el
+++ init.el
@@ -202,44 +202,18 @@
      )))
 
 ;; Organize centaur-tabs
-(defun centaur-tabs-buffer-groups ()
-  "`centaur-tabs-buffer-groups' control buffers' group rules.
+(defun vmacs-match (input)
+  (string-match-p (buffer-name) input))
 
-Group centaur-tabs with mode if buffer is derived from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
-All buffer name start with * will group to \"Emacs\".
-Other buffer group by `centaur-tabs-get-group-name' with project name."
+(defun centaur-tabs-buffer-groups ()
+  (interactive)
   (list
-   (cond
-    ((or (string-equal "*" (substring (buffer-name) 0 1))
-         (memq major-mode '(magit-process-mode
-                            magit-status-mode
-                            magit-diff-mode
-                            magit-log-mode
-                            magit-file-mode
-                            magit-blob-mode
-                            magit-blame-mode
-                            )))
-     "Emacs")
-    ((derived-mode-p 'prog-mode)
-     "Editing")
-    ((derived-mode-p 'dired-mode)
-     "Dired")
-    ((memq major-mode '(helpful-mode
-                        help-mode))
-     "Help")
-    ((memq major-mode '(org-mode
-                        org-agenda-clockreport-mode
-                        org-src-mode
-                        org-agenda-mode
-                        org-beamer-mode
-                        org-indent-mode
-                        org-bullets-mode
-                        org-cdlatex-mode
-                        org-agenda-log-mode
-                        diary-mode))
-     "OrgMode")
-    (t
-     (centaur-tabs-get-group-name (current-buffer))))))
+   (cond ((or (vmacs-match "*inferior-lisp")
+              (vmacs-match "*dashboard*")
+              (vmacs-match "*slime-repl sbcl")
+              (vmacs-match "*slime-events"))
+          "terminal-area")
+         (t "vmacs"))))
 
 
 ;;pd Treemacs
