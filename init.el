bb445d0edbe3b5276e6ac12c7228e316d274e30b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-09-03 04:44:40 -0400

    Remove cape

--- init.el
+++ init.el
@@ -398,9 +398,15 @@
 (global-diff-hl-mode)
 
 
+;;pd SLY
+;;note: Vmacs Common Lisp REPL
+(use-package sly :ensure t)
+(setq inferior-lisp-program "sbcl")
+
 ;;pd corfu
 ;;note: autocomplete
-(use-package corfu :ensure t)
+(use-package corfu :ensure t
+  :init (global-corfu-mode))
 (setq corfu-auto t)
 
 ;;! This is called this for a reason :)
@@ -411,55 +417,8 @@
         corfu-auto-delay  0 ;; TOO SMALL - NOT RECOMMENDED
         corfu-auto-prefix 1 ;; TOO SMALL - NOT RECOMMENDED
         completion-styles '(basic)))
-
-(global-corfu-mode)
-
-;;pd cape
-;;note: Common Lisp extensions into corfu
-;; Add extensions
-;; (use-package cape
-;;   :ensure t
-;;   ;; Bind dedicated completion commands
-;;   ;; Alternative prefix keys: C-c p, M-p, M-+, ...
-;;   :bind (("C-c p p" . completion-at-point) ;; capf
-;;          ("C-c p t" . complete-tag)        ;; etags
-;;          ("C-c p d" . cape-dabbrev)        ;; or dabbrev-completion
-;;          ("C-c p h" . cape-history)
-;;          ("C-c p f" . cape-file)
-;;          ("C-c p k" . cape-keyword)
-;;          ("C-c p s" . cape-elisp-symbol)
-;;          ("C-c p e" . cape-elisp-block)
-;;          ("C-c p a" . cape-abbrev)
-;;          ("C-c p l" . cape-line)
-;;          ("C-c p w" . cape-dict)
-;;          ("C-c p \\" . cape-tex)
-;;          ("C-c p _" . cape-tex)
-;;          ("C-c p ^" . cape-tex)
-;;          ("C-c p &" . cape-sgml)
-;;          ("C-c p r" . cape-rfc1345))
-;;   :init
-;;   ;; Add to the global default value of `completion-at-point-functions' which is
-;;   ;; used by `completion-at-point'.  The order of the functions matters, the
-;;   ;; first function returning a result wins.  Note that the list of buffer-local
-;;   ;; completion functions takes precedence over the global list.
-;;   (add-to-list 'completion-at-point-functions #'cape-dabbrev)
-;;   (add-to-list 'completion-at-point-functions #'cape-file)
-;;   (add-to-list 'completion-at-point-functions #'cape-elisp-block)
-;;   ;;(add-to-list 'completion-at-point-functions #'cape-history)
-;;   ;;(add-to-list 'completion-at-point-functions #'cape-keyword)
-;;   ;;(add-to-list 'completion-at-point-functions #'cape-tex)
-;;   ;;(add-to-list 'completion-at-point-functions #'cape-sgml)
-;;   ;;(add-to-list 'completion-at-point-functions #'cape-rfc1345)
-;;   ;;(add-to-list 'completion-at-point-functions #'cape-abbrev)
-;;   ;;(add-to-list 'completion-at-point-functions #'cape-dict)
-;;   ;;(add-to-list 'completion-at-point-functions #'cape-elisp-symbol)
-;;   ;;(add-to-list 'completion-at-point-functions #'cape-line)
-;;   )
-
-;;pd slime-company
-;;note: cape plugin for SLIME
-;; (use-package slime-company :ensure t)
-;; (add-to-list 'completion-at-point-functions (cape-company-to-capf #'company-slime))
+;; Only hit enter to select
+(define-key corfu-map [tab] nil)
 
 ;;pd kind-icon
 ;;note: nice icons for corfu autocomplete
@@ -471,12 +430,6 @@
   (kind-icon-default-face 'corfu-default) ; to compute blended backgrounds correctly
   :config
   (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
-
-;;pd SLY
-;;note: Vmacs Common Lisp REPL
-(use-package sly :ensure t)
-(setq inferior-lisp-program "sbcl")
-;; (setq-default sly-symbol-completion-mode nil)
 
 ;;pd Vmacs parentheses helper choice
 ;;note: 0 is off. Only set it to this if you like your code to look crazy.
