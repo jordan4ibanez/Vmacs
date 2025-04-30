141a712205333460128b485d0f2fb8ad899e58b2
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-30 17:27:30 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -14,12 +14,11 @@ print("startup time: " .. em.emacs_init_time())
 -- I don't feel like looking at the startup screen.
 em.set_intern("inhibit-startup-screen", 1)
 
-
+-- Make the GC not explode.
 local function startup_hook()
     em.setopt_intern({
         ["gc-cons-threshold"] = 800000,
         ["gc-cons-percentage"] = 0.1
     })
 end
-
 em.add_hook_intern(em.Hook.emacs_startup_hook, startup_hook, "startup_hook")
