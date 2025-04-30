62405ed48464cf35da67f6c9f15b922f6312f739
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-30 15:16:12 -0400

    smol fix

--- emacs/str.lua
+++ emacs/str.lua
@@ -2,9 +2,9 @@ local em = require("emacs")
 
 local str = {}
 
----     This predicate function does what string-match does, but it avoids modifying the match data.
-function str.string_match_p(regexp, str, optional, start)
-    return em.run("string-match-p", regexp, str, optional, start)
+--- This predicate function does what string-match does, but it avoids modifying the match data.
+function str.string_match_p(regexp, s, start)
+    return em.run("string-match-p", regexp, s, start) ~= nil
 end
 
 return str;
