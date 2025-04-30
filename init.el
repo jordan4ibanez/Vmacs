a08b30f3ed4930c08b00da0f522213d2fb6cfc8a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-29 23:53:38 -0400

    Make this look nicer

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;; Can't start the bootstrap without module support.
 (if (not (eq module-file-suffix nil))
-    (print "Has module support. Firing up!" #'external-debugging-output)
+    (princ "Has module support. Firing up!\n" #'external-debugging-output)
     (error "No module support. Recompile with module support."))
 
 
