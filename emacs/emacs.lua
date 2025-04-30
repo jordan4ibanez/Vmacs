85d9d9875a4c04dc7650f0dec2c9b20d87600910
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-30 16:16:06 -0400

    intern can produce emacs vars

--- emacs/emacs.lua
+++ emacs/emacs.lua
@@ -155,7 +155,7 @@ end
 --- This function returns the symbol in obarray whose name is name, or nil if obarray has no symbol with that name. Therefore, you can use intern-soft to test whether a symbol with a given name is already interned. If obarray is omitted, the value of the global variable obarray is used.
 --- The argument name may also be a symbol; in that case, the function returns name if name is interned in the specified obarray, and otherwise nil.
 function emacs.intern_soft(symbol_name)
-    return emacs.run("intern-soft", symbol_name) ~= nil
+    return emacs.run("intern-soft", symbol_name)
 end
 
 --- This function returns the string that is symbol’s name.
