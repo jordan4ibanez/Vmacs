8eddda8bf355046c8e038a4cc56a112e2f24a8cb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-29 22:16:35 -0400

    I remember how to use lisp :D

--- init.el
+++ init.el
@@ -1,1 +1,7 @@
+;; Can't start the bootstrap without module support.
+(if (not (eq module-file-suffix nil))
+    (print "Has module support. Firing up!" #'external-debugging-output)
+    (error "No module support. Recompile with module support."))
+
+
 (module-load "./liblua_mode.so")
