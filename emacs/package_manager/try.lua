4afcd7b11b796eac6aa43be2187e295d1c44d69a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-30 13:27:06 -0400

    Create try.lua

---
+++ emacs/package_manager/try.lua
@@ -0,0 +1,17 @@
+--- Number related functions.
+
+-- package.path = os.getenv("HOME") .. "/.emacs.d/emacs/?.lua;" .. package.path
+
+local em = require("emacs")
+
+em.require_interned("package")
+em.require_interned("url")
+
+local try = {}
+
+
+
+
+
+
+return try;
