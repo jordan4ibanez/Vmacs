e7a0e70f79fd78fb4f4297ae9ead22918d1d15b5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-30 15:17:23 -0400

    Alright, I'm more comfy, let's try some cool things

--- init.lua
+++ init.lua
@@ -7,47 +7,10 @@ local pakage = require("package")
 local com = require("commands")
 local buf = require("buffer")
 local num = require("numbers")
-local try = require("try")
+local prelude = require("prelude")
 
 print("startup time: " .. em.emacs_init_time())
 
 
-emacs.set(em.intern("inhibit-startup-screen"), 1)
+em.set_intern("inhibit-startup-screen", 1)
 
--- emacs.run("kill-buffer", emacs.run("get-buffer-create", "*Messages*"))
-
-local test_buffer = emacs.run("get-buffer-create", "*test*")
-
-
-emacs.run("switch-to-buffer", test_buffer)
-emacs.run("insert-into-buffer", test_buffer)
-
-local marker_point = em.intern("marker-point")
-
--- emacs.run("push-mark")
--- local start = emacs.run("region-beginning")
--- local ending = emacs.run("region-ending")
--- emacs.run("goto-char", start)
--- emacs.run("insert", "Hello from lua in D running in elisp! :D")
-
---! That's cool.
--- emacs.run("switch-to-buffer", emacs.run("get-buffer-create", "*scratch*"))
-
-
--- print("startup time: " .. em.emacs_init_time())
-
--- com.help_with_tutorial()
--- em.run(emacs_environment, "help-with-tutorial")
-
--- local function test_interactive()
---     emacs.run("print", "hi")
---     local x = emacs.run("read-number", "a:")
---     local y = emacs.run("read-number", "b:")
-
---     print(x + y)
---     print(num.zerop(x + y))
--- end
-
-
-
--- test_interactive()
