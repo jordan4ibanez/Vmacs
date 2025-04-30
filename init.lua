a65a96bbe4fe23c7c5917d313f3b8e8b86561db2
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-30 17:25:52 -0400

    And set gc functions

--- init.lua
+++ init.lua
@@ -11,11 +11,15 @@ local prelude = require("prelude")
 
 print("startup time: " .. em.emacs_init_time())
 
+-- I don't feel like looking at the startup screen.
 em.set_intern("inhibit-startup-screen", 1)
 
+
 local function startup_hook()
-    print("I am a startup hook!")
+    em.setopt_intern({
+        ["gc-cons-threshold"] = 800000,
+        ["gc-cons-percentage"] = 0.1
+    })
 end
 
 em.add_hook_intern(em.Hook.emacs_startup_hook, startup_hook, "startup_hook")
-
