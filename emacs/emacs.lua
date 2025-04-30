cec10f5f446f9e57d8451c29da207760c7bd6e78
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-30 15:24:03 -0400

    First wave

--- emacs/emacs.lua
+++ emacs/emacs.lua
@@ -21,7 +21,7 @@ end
 -- @param value Value to set
 -- @return value
 function emacs.set(object, value)
-    return functioncall(emacs_environment, "set", 2, { object, value })
+    return emacs.run("set", object, value)
 end
 
 --- Set a symbol to a value automatically interning the string.
@@ -29,7 +29,7 @@ end
 -- @param value Value to set
 -- @return value
 function emacs.set_intern(name, value)
-    return functioncall(emacs_environment, "set", 2, { emacs.intern(name), value })
+    return emacs.run(emacs_environment, "set", 2, { emacs.intern(name), value })
 end
 
 --- Set a symbols default value
@@ -37,26 +37,26 @@ end
 -- @param value Value to set
 -- @return value
 function emacs.set_default(name, value)
-    return functioncall(emacs_environment, "set-default", 2, { name, value })
+    return emacs.run(emacs_environment, "set-default", 2, { name, value })
 end
 
 --- Display a message inside emacs
 -- @param msg The message to be displayed
 function emacs.message(msg)
-    functioncall_no_return(emacs_environment, "message", 1, { msg })
+    emacs.run_no_return(emacs_environment, "message", 1, { msg })
 end
 
 --- Get the time emacs took to initialize
 -- @return String displayig timestamp, i.e. "1.12 seconds"
 function emacs.emacs_init_time()
-    return functioncall(emacs_environment, "emacs-init-time", 0, {})
+    return emacs.run(emacs_environment, "emacs-init-time", 0, {})
 end
 
 --- Create a symbol
 -- @param sym_name Name of the symbol
 -- @return A userdata object. ONLY USE THE RETURNED VALUE IN OTHER EMACS FUNCTIONS
 function emacs.intern(sym_name)
-    return functioncall(emacs_environment, "intern", 1, { sym_name })
+    return emacs.run(emacs_environment, "intern", 1, { sym_name })
 end
 
 --- Create a cons cell
@@ -75,7 +75,7 @@ end
 -- @param name Interned symbol of the list to append to
 -- @param value The value to append
 function emacs.add_to_list(list, value)
-    functioncall(emacs_environment, "add-to-list", 2, { list, value })
+    emacs.run(emacs_environment, "add-to-list", 2, { list, value })
 end
 
 --- Add a value to a list without triggering return value conversion
@@ -83,68 +83,68 @@ end
 -- @param name Interned symbol of the list to append to
 -- @param value The value to append
 function emacs.add_to_list_no_ret(list, value)
-    functioncall_no_return(emacs_environment, "add-to-list", 2, { list, value })
+    emacs.run_no_return(emacs_environment, "add-to-list", 2, { list, value })
 end
 
 --- Load a feature
 -- @param feature The feature to load
 function emacs.require(feature)
-    functioncall_no_return(emacs_environment, "require", 1, { feature })
+    emacs.run_no_return(emacs_environment, "require", 1, { feature })
 end
 
 --- Load a feature interned.
 -- @param feature The interned feature to load
 function emacs.require_interned(feature)
-    functioncall_no_return(emacs_environment, "require", 1, { emacs.intern(feature) })
+    emacs.run_no_return(emacs_environment, "require", 1, { emacs.intern(feature) })
 end
 
 --- Execute a file of lisp code
 -- @param file Name of file to load
 -- @return Returns true if the file exists and loads successfully
 function emacs.load(file)
-    return functioncall(emacs_environment, "load", 1, { file })
+    return emacs.run(emacs_environment, "load", 1, { file })
 end
 
 --- Evaluate elisp code
 -- @param form Elisp code to evaluate
 -- @return The return value of the evaluated code
 function emacs.eval(form)
-    return functioncall(emacs_environment, "eval", 1, { form })
+    return emacs.run(emacs_environment, "eval", 1, { form })
 end
 
 --- Evaluate elisp code
 -- @param form Elisp code to evaluate
 -- @return The return value of the evaluated code
 function emacs.eval_expression(form)
-    return functioncall(emacs_environment, "eval-expression", 1, { form })
+    return emacs.run(emacs_environment, "eval-expression", 1, { form })
 end
 
 function emacs.add_hook(hook, func, lm_local)
     if not lm_local then
-        functioncall_no_return(emacs_environment, "add-hook", 2, { hook, func })
+        emacs.run_no_return(emacs_environment, "add-hook", 2, { hook, func })
     else
-        functioncall_no_return(emacs_environment, "add-hook", 4, { hook, func, nil, true })
+        emacs.run_no_return(emacs_environment, "add-hook", 4, { hook, func, nil, true })
     end
 end
 
 function emacs.add_hook_intern(hook, func, lm_local)
     if not lm_local then
-        functioncall_no_return(emacs_environment, "add-hook", 2, { emacs.intern(hook), func })
+        emacs.run_no_return(emacs_environment, "add-hook", 2, { emacs.intern(hook), func })
     else
-        functioncall_no_return(emacs_environment, "add-hook", 4, { emacs.intern(hook), func, nil, true })
+        emacs.run_no_return(emacs_environment, "add-hook", 4, { emacs.intern(hook), func, nil, true })
     end
 end
 
 function emacs.make_local_variable(symbol)
-    return functioncall(emacs_environment, "make-local-variable", 1, { symbol })
+    return emacs.run(emacs_environment, "make-local-variable", 1, { symbol })
 end
 
 function emacs.kbd(keys)
-    return functioncall(emacs_environment, "kbd", 1, { keys })
+    return emacs.run(emacs_environment, "kbd", 1, { keys })
 end
 
 function emacs.define_key(keymap, key, def)
-    functioncall_no_return(emacs_environment, "define-key", 3, { keymap, key, def })
+    emacs.run_no_return(emacs_environment, "define-key", 3, { keymap, key, def })
 end
 
 --- This function returns t if object is a symbol, nil otherwise.
