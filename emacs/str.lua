9d83be25004b13874f03154fae3a086b1279bbdd
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-30 13:38:36 -0400

    whoops

--- emacs/str.lua
+++ emacs/str.lua
@@ -1,10 +1,10 @@
 local em = require("emacs")
 
-local string = {}
+local str = {}
 
 ---     This predicate function does what string-match does, but it avoids modifying the match data.
-function string.string_match_p(regexp, str, optional, start)
+function str.string_match_p(regexp, str, optional, start)
     return em.run("string-match-p", regexp, str, optional, start)
 end
 
-return string;
+return str;
