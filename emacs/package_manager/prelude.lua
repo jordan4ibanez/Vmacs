35276f98bd4d88b2ff381ab12211c7bf593c7e47
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-01 12:36:08 -0400

    Make the lua doc easier to read

--- emacs/package_manager/prelude.lua
+++ emacs/package_manager/prelude.lua
@@ -60,7 +60,9 @@ local function booleanize(input)
 end
 
 --- Super function.
---- This is a lua integration of the standard elisp use-package lambda.
+---
+--- This is an integration of the standard elisp use-package lambda into lua 5.2.
+---
 --- You might want to read the code to see how to use this.
 function prelude.use_package(package_name, def)
     -- If you're having trouble understanding any of this:
